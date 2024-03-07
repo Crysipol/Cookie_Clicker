4fb3e6674640972a61dcc75d9f041d5517b1bb5a
Kevin Paute <Kevin.Paute2@gmail.com>
2024-03-07 12:06:53 -0500

    Update GameMangerScript.cs

--- Assets/GameMangerScript.cs
+++ Assets/GameMangerScript.cs
@@ -7,10 +7,11 @@ public class GameMangerScript : MonoBehaviour
     public GameObject Cookie;
     public GameObject crusor;
     public GameObject grandma;
-    public int cookiecount;
-    public int cookieadd;
-    public bool clicks;
+    public GameObject farm;
     public GameObject explosionPrefab;
+    public ulong cookiecount;
+    public uint cookieadd;
+    public bool clicks;
 
 
     // Sets Cookiecount to zero while spawning a cookie and playing a sound
@@ -32,19 +33,37 @@ public class GameMangerScript : MonoBehaviour
             cookiecount = cookieadd + cookiecount;
         }
 
+        if(cookiecount % 10 = 0 && cookiecount != 0)
+        {
+            cookiecount * 2;
+        }
+        
         if(cookiecount == 10 && clicks == false)
         {
             Debug.Log("You got a Cursor! It begains making additional cookies!");
-            
-            cookieadd = 2;
             clicks = true;
-            
             GetComponent<AudioSource>().Play();
-
             Instantiate(crusor);
             Instantiate(explosionPrefab);
         }
-
+        
+        if(cookiecount == 20 && clikcs == true)
+        {
+            Debug.Log("You got a grandma! She begins making additional cookies!");
+            clicks = false;
+            GetComponent<AudioSource>().Play();
+            Instantiate(grandma);
+            Instantiate(explosionPrefab);
+        }
+        
+        if(cookiecount == 30 && clicks == false)
+        {
+            Debug.Log("You got a cookie farm! It begins producing additional cookies!");
+            clicks = true;
+            GetComponent<AudioSource>().Play();
+            Instantiate(farm);
+            Instantiate(explosionPrefab);
+        }
 
     }
 }
